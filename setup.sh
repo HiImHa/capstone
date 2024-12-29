0934481b24302a9af2864f372f24e5b225c0cfc6
Ngoc Ha Phan <ngochaphan.work@gmail.com>
2024-12-29 12:04:23 +0700

    update

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DATABASE_URL="postgresql://capstone_d75w_user:jYiDwuMO43B6bz2Fdx1zfXob20FJcc69@dpg-ctoc46dsvqrc73b7j6ug-a.oregon-postgres.render.com/capstone_d75w"
+export EXCITED="true"
+export FLASK_APP=app.py
+echo "setup.sh script executed successfully!"
